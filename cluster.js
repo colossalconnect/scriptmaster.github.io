331ee14ef917327127a39f7fe9dbe6bd2d73c9f1
colossalconnect <gourab@colossalconnect.com>
2014-02-17 02:32:04 +0530

    Create cluster.js

---
+++ cluster.js
@@ -0,0 +1,26 @@
+var http	= require('http'),
+	  express	= require('express'),
+    path	= require('path'),
+    swig	= require('swig'),
+    os		= require('os'),
+    fs		= require('fs'),
+    cluster	= require('cluster'),
+    os		= require('os'),
+    cpus	= os.cpus().length;
+
+
+if (cluster.isMaster) {
+    console.log('\nTotal number of cores: ' + numCPUs);
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log(new Date() + ': Worker ' + worker.process.pid + ' exited. Code: '+code+', Signal: '+signal);
+        cluster.fork();
+    });
+
+    master();
+} else {
+    worker();
+}
